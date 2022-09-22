122656c92423daf16f786e5eca2a78602b13bb30
Ian Mihura <mihura.ian@gmail.com>
2022-09-22 12:28:25 +0200

    object

--- main.py
+++ main.py
@@ -1,55 +1,76 @@
-graph = {
-    "SB": 4,
-    "SC": 2,
-    "BC": 1,
-    "BD": 5,
-    "CD": 8,
-    "CE": 10,
-    "DT": 6,
-    "DE": 2,
-    "ET": 3
-}
+import json
 
 
-def loop(g, c, t, s, weight, order, done):
-    keys = [x.find(c) for x in g]
-    links = {i.replace(c,""):graph[i] for x, i in enumerate(graph) if keys[x] > -1}
-    links = {x:links[x] for x in links if not done[x]}
+class Graph:
+    def __init__(self, graph: dict, start_node = "S", end_node = "T"):
+        """Params:
+        graph of the shape: {'AB': 2, ...}
+            A & B being nodes conected with an edge of weight 2
+        start_node & end_node single char string"""
 
-    for x in links:
-        if weight[x] > weight[c] + links[x]:
-            weight[x] = weight[c] + links[x]
-            order[x] = c
+        self.g = graph
+        self.s = start_node
+        self.t = end_node
 
-    done[c] = True
+        self.weight = {}
+        self.order = {}
+        self.done = {}
 
-    sort = sorted(weight.items(), key=lambda item: item[1])
-    nc = [k[0] for k in sort if not done[k[0]]][0]
-
-    if nc is not t:
-        loop(g, nc, t, s, weight, order, done)
-    else:
-        print(viz(order, t, s))
-
-
-def viz(o, n, s):
-    if n is not s:
-        return viz(o, o[n], s) + " -> " + n
-    else:
-        return s
+        keys = set(''.join(self.g.keys()))
+        self.order = {k:'' for k in keys}
+        self.done = {k:False for k in keys}
+        self.weight = {k:(999999 if k is not self.s else 0) for k in keys}
 
 
-def start(g, s = "S", t = "T"): 
-    weight = {}
-    order = {}
-    done = {}
-
-    keys = set(''.join(graph.keys()))
-    order = {k:'' for k in keys}
-    done = {k:False for k in keys}
-    weight = {k:(999999 if k is not s else 0) for k in keys}
-
-    loop(g, s, t, s, weight, order, done)
+    def __call__(self):
+        self._loop(self.s)
 
 
-start(graph)
+    def _loop(self, c: str):
+        """Internal funcion. 
+        Takes a current node for evaluation c (single char string)"""
+        keys = [x.find(c) for x in self.g]
+        links = {i.replace(c,""):self.g[i] for x, i in enumerate(self.g) if keys[x] > -1}
+        links = {x:links[x] for x in links if not self.done[x]}
+
+        for x in links:
+            if self.weight[x] > self.weight[c] + links[x]:
+                self.weight[x] = self.weight[c] + links[x]
+                self.order[x] = c
+
+        self.done[c] = True
+
+        sort = sorted(self.weight.items(), key=lambda item: item[1])
+        nc = [k[0] for k in sort if not self.done[k[0]]][0]
+
+        if nc is not self.t:
+            self._loop(nc)
+
+
+    def __str__(self):
+        return self._viz(self.t)
+
+
+    def __repr__(self):
+        return f'Graph(graph={self.g}, path="{self._viz(self.t)}", path_weight={self.weight[self.t]})'
+
+
+    def _viz(self, n: str):
+        """Internal function.
+        Takes a node n that is the last node of the path (single char string)"""
+        if n is not self.s:
+            return self._viz(self.order[n]) + " -> " + n
+        else:
+            return self.s
+
+
+if __name__ == "__main__":
+    with open('graph.json') as f:
+        json_graph = json.load(f)
+
+    graph = Graph(json_graph)
+    graph()
+    print(graph.__repr__())
+    # print(graph.order)
+    # print(graph.weight)
+    # print(graph.)
